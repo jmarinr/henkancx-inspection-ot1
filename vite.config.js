18391d5e39f376f980559d444851bce8513b4d60
jmarinr <jmarinr@users.noreply.github.com>
2025-11-11 23:58:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ Configuración correcta para build y GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  root: '.',               // asegúrate que el index.html está en la raíz
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  base: './'               // ruta relativa para GitHub Pages
 })
